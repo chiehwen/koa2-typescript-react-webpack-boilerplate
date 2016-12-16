ff6abce8e16a10d24758abb019222fb7dd99072e
Chieh-Wen Yang <chuck.yang@hackathon.tw>
2016-12-16 19:10:20 +0800

    successful stage.

--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,18 @@ const path = require('path');
 
 module.exports = {
 
-  entry: './src/server.ts',
+  entry: './src/index.tsx',
   target: 'node',
   output: {
     path: path.join(__dirname, 'dist'),
     publicPath: '/dist/',
-    filename: 'server.bundle.js'
+    filename: 'bundle.js'
   },
   module: {
     rules: [
       { exclude: /node_modules/ },
       // All files with a '.ts' extension will be handled
-      { test: /\.ts$/, use: [{ loader: 'ts-loader' }]},
+      { test: /\.tsx?$/, use: [{ loader: 'ts-loader' }]},
       { test: /\.json$/, use: [{ loader: 'json-loader' }]}
     ]
   },
@@ -23,6 +23,10 @@ module.exports = {
   devtool: "source-map",
   resolve: {
     // Add '.js' and 'ts' as resolvable extensions.
-    extensions: [ '*','.js', '.ts' ]
-  }
+    extensions: [ '*','.js', '.ts', '.tsx' ]
+  },
+  externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    }
 };
